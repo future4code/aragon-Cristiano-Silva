7d397f230def0d03a8e1b67e2a7402eb1ce3fdc3
Cristiano Silva <crizsilv4@gmail.com>
2022-04-21 00:43:11 -0300

    Projeto insta4 comentarios

--- modulo2/estados-insta4/insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ modulo2/estados-insta4/insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -14,10 +14,13 @@ const InputComentario = styled.input`
 
 export class SecaoComentario extends Component {
 	state = {
-
+		comentario:""
 	}
 
-	onChangeComentario() {
+	onChangeComentario =(event) =>{
+		this.setState({
+			comentario: event.target.value
+		})
 
 	}
 
@@ -25,7 +28,7 @@ export class SecaoComentario extends Component {
 		return <CommentContainer>
 			<InputComentario
 				placeholder={'Comentário'}
-				value={''}
+				value={this.state.comentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
