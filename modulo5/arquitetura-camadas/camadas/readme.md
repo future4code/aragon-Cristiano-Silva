e743d1e8913117308db3cdcdc53f41bba6ebadb5
cristiano silva <crizsilv4@gmail.com>
2022-08-08 17:45:12 -0300

    exercicio-arquitetura-camadas

---
+++ modulo5/arquitetura-camadas/camadas/readme.md
@@ -0,0 +1,31 @@
+# Projeto Cookenu
+
+Sistema simples de Dados gerais de Usuários.
+
+## Apêndice
+
+Este sistema possui o cadastro de um usuário pelo id, name, password, email e role indicando se o mesmo é NORMAL ou ADMIN, um login com Email e password, listagem de usuários com seus dados e também a possibilidade de poder deleta um usuário pela ID.
+
+## Documentação da API
+
+
+## Stacks
+
+NodeJs
+TypeScript
+SQl
+Knex
+Express
+Cors
+Poo
+
+
+## Referência
+
+ - [Projeto LabeSystem](https://github.com/future4code/aragon-Cristiano-Silva/pull/62)
+ - [Projeto Labeccommerce](https://github.com/future4code/aragon-Cristiano-Silva/pull/60)
+
+
+## Autores
+
+- [aragon-Cristiano-Silva](https://github.com/future4code/aragon-Cristiano-Silva/pull/63)
---
+++ modulo5/arquitetura-camadas/camadas/src/business/UserBusiness.ts
@@ -0,0 +1,201 @@
+import { Authenticator, ITokenPayload } from './../services/Authenticator';
+import { UserDatabase } from './../database/UserDatabase';
+import { User, USER_ROLES } from './../models/User';
+import { HashManager } from './../services/HashManager';
+import { IdGenerator } from './../services/IdGenerator';
+
+
+export class UserBusiness {
+
+    public signup = async (input: any) =>{
+        const name = input.name
+        const email = input.email 
+        const password = input.password
+
+        if (!name || !email || !password) {
+            throw new Error("Parâmetros faltando")
+        }
+
+        if (typeof name !== "string") {
+            throw new Error("Parâmetro 'nickname' deve ser uma string")
+        }
+
+        if (typeof email !== "string") {
+            throw new Error("Parâmetro 'email' deve ser uma string")
+        }
+
+        if (typeof password !== "string") {
+            throw new Error("Parâmetro 'password' deve ser uma string")
+        }
+
+        if (name.length < 3) {
+            throw new Error("O parâmetro 'nickname' deve possuir ao menos 3 caracteres")
+        }
+
+        if (password.length < 6) {
+            throw new Error("O parâmetro 'password' deve possuir ao menos 6 caracteres")
+        }
+
+        if (!email.includes("@") || !email.includes(".com")) {
+            throw new Error("O parâmetro 'password' deve possuir ao menos 6 caracteres")
+        }
+
+
+        const idGenerator = new IdGenerator()
+        const id = idGenerator.generate()
+
+        const hashManager = new HashManager()
+        const hashPassword = await hashManager.hash(password)
+
+        const user = new User(
+            id,
+            name,
+            email,
+            hashPassword,
+            USER_ROLES.NORMAL
+        )
+
+        const userDatabase = new UserDatabase()
+        await userDatabase.createUser(user)
+
+        const payload : ITokenPayload = {
+            id:user.getId(),
+            role: user.getRole()
+        }
+
+        const authenticator = new Authenticator()
+        const token = authenticator.generateToken(payload)
+
+        const response = {
+            message: "Cadastro realizado com sucesso",
+            token
+        }
+
+        return response
+    }
+
+    public login = async (input: any) =>{
+        const email = input.email
+        const password = input.password
+
+        
+        if (!email || !password) {
+            throw new Error("Email ou senha faltando")
+        }
+
+        if (typeof email !== "string") {
+            throw new Error("Parâmetro 'email' deve ser uma string")
+        }
+
+        if (typeof password !== "string") {
+            throw new Error("Parâmetro 'password' deve ser uma string")
+        }
+
+        if (password.length < 6) {
+            throw new Error("O parâmetro 'password' deve possuir ao menos 6 caracteres")
+        }
+
+        if (!email.includes("@") || !email.includes(".com")) {
+            throw new Error("O parâmetro 'password' deve possuir ao menos 6 caracteres")
+        }
+
+        const userDatabase = new UserDatabase()
+        const userDB = await userDatabase.findByEmail(email)
+
+        const user = new User (
+            userDB.id,
+            userDB.name,
+            userDB.email,
+            userDB.password,
+            userDB.role
+
+        )
+
+        const hashManager = new HashManager()
+        const isPasswordCorrect = await hashManager.compare(
+            password,
+            user.getPassword()
+        )
+
+        if(!isPasswordCorrect){
+            throw new Error("Senha inválida")
+        }
+
+        const payload: ITokenPayload = {
+            id: user.getId(),
+            role: user.getRole()
+        }
+
+        const authenticator = new Authenticator()
+        const token = authenticator.generateToken(payload)
+
+        const response = {
+            message: "Login realizado com sucesso",
+            token
+        }
+
+        return response
+    }
+
+    public getAllUsers = async (input: any) =>{
+        const token = input.token
+        const search = input.search || ""
+        
+
+        const authenticator = new Authenticator()
+        const payload = authenticator.getTokenPayload(token)
+
+        if(!payload){
+            throw new Error("Token invalido")
+        }
+
+        const userDataBase = new UserDatabase()
+        const usersDB = await userDataBase.getUsers(
+            search
+        )
+
+        const users = usersDB.map((user) =>{
+            return new User(
+                user.id,
+                user.name,
+                user.email,
+                user.password,
+                user.role
+            )
+        })
+        const response = {
+            users
+        }
+
+        return response
+
+        }
+  
+        public deleteUser = async (input: any) =>{
+            const token = input.token
+            const id = input.id
+
+            const authenticator = new Authenticator()
+            const payload = authenticator.getTokenPayload(token)
+
+            const userDatabase = new UserDatabase()
+            const userDB = await userDatabase.findById(id)
+
+            const userToDelete = new User(
+                userDB.id,
+                userDB.name,
+                userDB.email,
+                userDB.password,
+                userDB.role
+            )
+
+            await userDatabase.deleteUserById(userToDelete.getId())
+
+            const response = {
+                message: "Usuario deletado com sucesso"
+            }
+
+            return response
+        }
+
+}
